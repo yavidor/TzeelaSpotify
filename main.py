3adc4532b346554a862fb6475bd9d72f41b91e6b
yavidor <yonatan@avidor.org>
2022-09-26 14:27:06 +0300

    Sorted

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 import pandas as pd
+
 URL = 'https://api.spotify.com'
 token = 'BQDXLRoZ2aJSEIvUdGabTfwbs48Rvi_EKOhRrp_i-qQtryvTA8xU00JXR6oDgZLE-nA_5Q5EpQcra4RxCohim77loqTdcwEbdQxVTRSPOAxvdlglJpGljPUxWP3awRKj8ZVxj1EKsF0cHFcLoviT5zx-HctLXxp0rFjCmU8HZSDmenJ7kj-1KLXTBmqXETs-N_G_WOr1AmVe-MScX1Bm_VQhwRcOvXUrmL9AE8B42oxPprAAOROdELEG5g2Ix9zDvXGdLRKnei3dPepB4c9c9iU9Xeo5GehV7RU8HOLs3wxVktkFPQ0YfJ_b_uW5NPgubZQnVyLIk9Fe'
 auth = {'Authorization': f'Bearer {token}'}
@@ -24,13 +25,13 @@ def extract_songs(items):
         if track_name not in songs_dict:
             songs_dict[track_name] = 0
         songs_dict[track_name] += int(item['track']['duration_ms']) / 60000
-    return pd.DataFrame(list(songs_dict.items()),columns=['Name','Minutes'])
+    return pd.DataFrame(list(songs_dict.items()), columns=['Name', 'Minutes'])
 
 
 def main():
     track_list = get_listened([], 0)
     track_list = extract_songs(track_list)
-    track_list.sort_values(by=['Minutes'])
+    track_list = track_list.sort_values(by=['Minutes'], ascending=False)
     print(track_list)
     print(len(track_list))
     track_list.to_csv('file.csv')
